e71aacdb27efdea33ecf2fb8a361f11b97ce4102
Abigail-Cobbina <a.amaopooco@alustudent.com>
2026-06-28 03:40:09 -0700

    quest 21 added

---
+++ quests/quest_21_reusable_incantation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def greet_adventurer():
+    print("Greetings, adventurer! May your code be bug-free and your quests legendary!")
+
+
+greet_adventurer()
+greet_adventurer()
+greet_adventurer()

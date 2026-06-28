05da3f7cbbda825d6310eadb8c16ab30e5594532
Yom Deng <y.deng@alustudent.com>
2026-06-28 15:01:24 +0200

    Add Quest 23: The Oracle's Vision

---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Quest 23: The oracle's vision - returns
+
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+rectangle_1_area = calculate_area(5, 3)
+rectangle_2_area = calculate_area(10, 7)
+
+print(f"Rectangle 1 (5 x 3) has an area of {rectangle_1_area}.")
+print(f"Rectangle 2 (10 x 7) has an area of {rectangle_2_area}.")
